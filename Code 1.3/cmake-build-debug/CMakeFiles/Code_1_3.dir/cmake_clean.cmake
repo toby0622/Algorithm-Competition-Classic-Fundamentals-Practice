edcf3641120f73203ac56497fccad5fa8648d0ea
Chi-Wen "Toby" Teng <52705034+toby0622@users.noreply.github.com>
2020-05-26 23:37:41 +0800

    Add files via upload

---
+++ Code 1.3/cmake-build-debug/CMakeFiles/Code_1_3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Code_1_3.dir/main.c.obj"
+  "Code_1_3.exe"
+  "Code_1_3.exe.manifest"
+  "Code_1_3.pdb"
+  "libCode_1_3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Code_1_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Code 1.3/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    printf("%.8f\n", 1 + 2 * sqrt(3) / (5 - 0.1));
+
+    return 0;
+}
+
+// integer - float = float (integer change to float, and become float - float = float)
+
+// function sqrt is for calculating the square root of the specific number
