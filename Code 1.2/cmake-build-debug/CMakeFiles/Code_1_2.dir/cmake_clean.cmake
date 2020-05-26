33ef11a30cbbc89b6c1c13c0c0bb0aa6a83fcf06
Chi-Wen "Toby" Teng <52705034+toby0622@users.noreply.github.com>
2020-05-26 23:37:02 +0800

    Add files via upload

---
+++ Code 1.2/cmake-build-debug/CMakeFiles/Code_1_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Code_1_2.dir/main.c.obj"
+  "Code_1_2.exe"
+  "Code_1_2.exe.manifest"
+  "Code_1_2.pdb"
+  "libCode_1_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Code_1_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
